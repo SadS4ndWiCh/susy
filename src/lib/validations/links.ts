6a566d69c0a8dc8f7cab2127781481fae094bcbe
sads4ndwich <caiocamargo007@gmail.com>
2024-01-19 18:14:13 -0300

    refactor: added more detailed link validation

--- src/lib/validations/links.ts
+++ src/lib/validations/links.ts
@@ -1,5 +1,13 @@
 import { z } from "zod";
 
-export const newLinkSchema = z.object({
+export const linkSchema = z.object({
+  id: z.string(),
   url: z.string().url(),
+  susLink: z.string(),
+  expiresAt: z.string(),
+  createdAt: z.string()
 });
+export type Link = z.infer<typeof linkSchema>;
+
+export const newLinkSchema = linkSchema.pick({ url: true });
+export type NewLink = z.infer<typeof newLinkSchema>;
