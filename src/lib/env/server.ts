9757fd9ccf504fdbb2f3d93cea6ea4bc5bc965c8
sads4ndwich <caiocamargo007@gmail.com>
2024-01-20 16:01:18 -0300

    feat(env): include NODE_ENV

--- src/lib/env/server.ts
+++ src/lib/env/server.ts
@@ -3,10 +3,12 @@ import { createEnv } from "@t3-oss/env-nextjs";
 
 export const env = createEnv({
   server: {
+    NODE_ENV: z.enum(["development", "production", "test"]),
     DATABASE_URL: z.string().url(),
     DATABASE_AUTH_TOKEN: z.string(),
   },
   runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
     DATABASE_AUTH_TOKEN: process.env.DATABASE_AUTH_TOKEN,
   }
