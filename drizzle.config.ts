783cca107ee7e46f56465413d12ffb13a9d1341e
sads4ndwich <caiocamargo007@gmail.com>
2024-01-20 00:26:20 -0300

    refactor: use default env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,6 @@
-import { env } from "@/env";
+import { config } from "dotenv";
+config({ path: ".env.local" });
+
 import type { Config } from "drizzle-kit";
 
 export default {
@@ -6,7 +8,7 @@ export default {
   schema: "src/db/schemas",
   driver: "turso",
   dbCredentials: {
-    url: env.DATABASE_URL,
-    authToken: env.DATABASE_AUTH_TOKEN
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN!
   }
 } satisfies Config;
