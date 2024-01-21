28c547214a794463603e2bcfc70d25a8e774bdf7
sads4ndwich <caiocamargo007@gmail.com>
2024-01-20 22:42:01 -0300

    chore(migration): added `idle_expires` field into `user_sessions` table

--- src/db/schemas/user-sessions.ts
+++ src/db/schemas/user-sessions.ts
@@ -5,7 +5,8 @@ import { users } from ".";
 export const userSessions = sqliteTable("user_sessions", {
   id: text("id").notNull().primaryKey(),
   userId: text("user_id", { length: 15 }).notNull(),
-  activeExpires: text("active_expires").notNull()
+  activeExpires: text("active_expires").notNull(),
+  idleExpires: text("idle_expires").notNull()
 });
 
 export const userSessionsRelations = relations(userSessions, ({ one }) => ({
