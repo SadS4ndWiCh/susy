e55096041a4300fa82c0052d61bf3921ec002274
sads4ndwich <caiocamargo007@gmail.com>
2024-01-21 17:34:52 -0300

    style(pages): added space between header content and logout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-import { CreateLinkForm } from "@/components/create-link-form";
 import { LogoutButton } from "@/components/logout-button";
+import { CreateLinkForm } from "@/components/create-link-form";
 import { UserLinks } from "@/components/user-links";
 
 export default function Home() {
   return (
     <>
-      <header className="max-w-xl mx-auto mt-8">
-        <h1 className="text-xl font-bold">susy.</h1>
-        <p className="text-muted-foreground">
-          Already have many URL shortener boring solutions, so aiming for a more 
-          funny one, now you can create a suspicious link to share.
-        </p>
+      <header className="flex justify-between gap-4 max-w-xl mx-auto mt-8">
+        <div>
+          <h1 className="text-xl font-bold">susy.</h1>
+          <p className="text-muted-foreground">
+            Already have many URL shortener boring solutions, so aiming for a more 
+            funny one, now you can create a suspicious link to share.
+          </p>
+        </div>
 
         <LogoutButton />
       </header>
