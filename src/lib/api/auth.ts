1afcf28b127d80bbe76464fe46efb900a223490b
sads4ndwich <caiocamargo007@gmail.com>
2024-01-20 23:25:41 -0300

    feat(api-client): added api client functions

---
+++ src/lib/api/auth.ts
@@ -0,0 +1,29 @@
+import type { SignUp, SignIn } from "@/lib/validations/auth";
+
+import { api } from ".";
+
+export async function signUp(credentials: SignUp) {
+  return await api("/api/auth/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(credentials)
+  });
+}
+
+export async function signIn(credentials: SignIn) {
+  return await api("/api/auth/signin", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(credentials)
+  });
+}
+
+export async function signOut() {
+  return await api("/api/auth/signout", {
+    method: "POST"
+  });
+}
