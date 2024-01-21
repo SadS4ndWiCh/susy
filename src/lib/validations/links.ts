74d43c6c215aee0ab902421f34bfb8565676db4c
sads4ndwich <caiocamargo007@gmail.com>
2024-01-21 01:20:31 -0300

    feat(validation): added all links schema validation

--- src/lib/validations/links.ts
+++ src/lib/validations/links.ts
@@ -4,10 +4,14 @@ export const linkSchema = z.object({
   id: z.string(),
   url: z.string().url(),
   susLink: z.string(),
+  ownerId: z.string(),
   expiresAt: z.string(),
   createdAt: z.string()
 });
 export type Link = z.infer<typeof linkSchema>;
+
+export const allLinksSchema = z.array(linkSchema);
+export type AllLinks = z.infer<typeof allLinksSchema>;
 
 export const newLinkSchema = linkSchema.pick({ url: true });
 export type NewLink = z.infer<typeof newLinkSchema>;
