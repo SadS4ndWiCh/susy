f870f17587e01a897b5f828579574eb9e6147871
sads4ndwich <caiocamargo007@gmail.com>
2024-01-22 20:43:17 -0300

    fix(database): wrong default value in `users.created_at` as string of CURRENT_TIME

--- src/lib/server/db/schemas/users.ts
+++ src/lib/server/db/schemas/users.ts
@@ -1,12 +1,13 @@
-import { relations } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+
 import { links } from ".";
 
 export const users = sqliteTable("users", {
   id: text("id", { length: 15 }).notNull().primaryKey(),
   username: text("username", { length: 64 }).notNull(),
   email: text("email", { length: 255 }).notNull().unique(),
-  createdAt: text("created_at").notNull().default("CURRENT_TIMESTAMP")
+  createdAt: text("created_at").notNull().default(sql`CURRENT_TIMESTAMP`)
 });
 
 export const usersRelations = relations(users, ({ many }) => ({
