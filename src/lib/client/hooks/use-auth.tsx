f90643e910cb7087a221e6ebe04e670b59ff9517
sads4ndwich <caiocamargo007@gmail.com>
2024-01-21 22:10:50 -0300

    feat(hooks): create `useAuth` hook to handle auth and redirects

---
+++ src/lib/client/hooks/use-auth.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
+import { toast } from "sonner";
+
+import * as api from "@/lib/client/api/auth"
+import { SignIn, SignUp } from "@/lib/shared/validations/auth";
+
+export function useAuth() {
+  const router = useRouter();
+
+  const signUp = async (credentials: SignUp) => {
+    const res = await api.signIn(credentials);
+
+    if (res.ok) {
+      return router.replace("/");
+    }
+
+    toast.error("Failed to sign up");
+  }
+
+  const signIn = async (credentials: SignIn) => {
+    const res = await api.signIn(credentials);
+
+    if (res.ok) {
+      return router.replace("/");
+    }
+
+    toast.error("Failed to sign in");
+  }
+
+  const signOut = async () => {
+    const res = await api.signOut();
+
+    if (res.ok) {
+      return router.replace("/signin");
+    }
+
+    toast.error("Failed to signout");
+  }
+
+  return { signUp, signIn, signOut };
+}
