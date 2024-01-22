2620d8e77d99c11d0885a61a9a656c6692d6adce
sads4ndwich <caiocamargo007@gmail.com>
2024-01-22 13:25:44 -0300

    feat(middleware): create new session cookie if session freshed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import { validateRequest } from "@/lib/server/auth/request";
+import { createSessionCookie } from "@/lib/server/auth/session";
 
 export async function middleware(req: NextRequest) {
   const session = await validateRequest(req);
@@ -9,13 +10,17 @@ export async function middleware(req: NextRequest) {
   const isAuthPage = req.nextUrl.pathname.startsWith("/signup") ||
                      req.nextUrl.pathname.startsWith("/signin");
 
-  if (isAuthPage) {
-    if (isAuth) {
-      return NextResponse.redirect(new URL("/", req.url));
-    }
+  if (isAuthPage && !isAuth) return null;
 
-    return null;
+  if (isAuthPage && isAuth && session.fresh) {
+    return NextResponse.redirect(new URL("/", req.url), {
+      headers: {
+        "Set-Cookie": await createSessionCookie(session)
+      }
+    });
   }
+
+  if (isAuthPage && isAuth) return NextResponse.redirect(new URL("/", req.url));
 
   if (!isAuth) {
     let from = req.nextUrl.pathname;
