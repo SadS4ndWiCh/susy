9588e8d3bd7395a40972a86cd832894eb84e209a
sads4ndwich <caiocamargo007@gmail.com>
2024-01-23 16:34:40 -0300

    refactor(validation-auth): use `users` schema

--- src/lib/shared/validations/auth.ts
+++ src/lib/shared/validations/auth.ts
@@ -1,10 +1,8 @@
 import { z } from "zod";
+import { userAttributesSchema } from "./users";
 
-export const signupSchema = z.object({
-  username: z.string().min(3).max(64),
-  email: z.string().email().max(255),
-  password: z.string().min(6).max(255)
-});
+export const signupSchema = userAttributesSchema
+  .extend({ password: z.string().min(6).max(255) });
 export type SignUp = z.infer<typeof signupSchema>;
 
 export const signinSchema = signupSchema.omit({ username: true });
