fc945524219b1b72c725303cec8c159f9668f5c5
sads4ndwich <caiocamargo007@gmail.com>
2024-01-24 11:34:05 -0300

    feat(api-users): added update and delete user route

--- src/app/(api)/api/users/route.ts
+++ src/app/(api)/api/users/route.ts
@@ -5,8 +5,9 @@ import { eq } from "drizzle-orm";
 import { db } from "@/lib/server/db/connection";
 import { users } from "@/lib/server/db/schemas";
 import { validateRequest } from "@/lib/server/auth/request";
+import { deleteUser, updateUserAttributes } from "@/lib/server/auth/users";
 
-import type { User } from "@/lib/shared/validations/users";
+import { updatableAttributesSchema, type User } from "@/lib/shared/validations/users";
 
 export async function GET(req: Request) {
   const session = await validateRequest(req);
@@ -41,4 +42,42 @@ export async function GET(req: Request) {
   }
 
   return NextResponse.json(user[0]);
+}
+
+export async function PUT(req: Request) {
+  const session = await validateRequest(req);
+  if (!session) {
+    return NextResponse.json(
+      { success: false, message: "Unauthorized" },
+      { status: 401 }
+    );
+  }
+
+  const body = await req.json();
+
+  const validatedAttributes = updatableAttributesSchema.safeParse(body);
+  if (!validatedAttributes.success) {
+    return NextResponse.json(
+      { success: false, message: "Invalid request body" },
+      { status: 400 }
+    );
+  }
+
+  await updateUserAttributes(session.userId, validatedAttributes.data);
+
+  return NextResponse.json({ success: true });
+}
+
+export async function DELETE(req: Request) {
+  const session = await validateRequest(req);
+  if (!session) {
+    return NextResponse.json(
+      { success: false, message: "Unauthorized" },
+      { status: 401 }
+    );
+  }
+
+  await deleteUser(session.userId);
+
+  return NextResponse.json({ success: true });
 }
