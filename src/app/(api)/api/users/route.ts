cb3e2a65194530d936fd922e5d4e4fcf596e71a3
sads4ndwich <caiocamargo007@gmail.com>
2024-01-23 23:05:18 -0300

    feat(api): create users api route

---
+++ src/app/(api)/api/users/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+
+import { eq } from "drizzle-orm";
+
+import { db } from "@/lib/server/db/connection";
+import { users } from "@/lib/server/db/schemas";
+import { validateRequest } from "@/lib/server/auth/request";
+
+import type { User } from "@/lib/shared/validations/users";
+
+export async function GET(req: Request) {
+  const session = await validateRequest(req);
+  if (!session) {
+    return NextResponse.json(
+      { success: false, message: "Unauthorized" },
+      { status: 401 }
+    );
+  }
+
+  let user: User[];
+  try {
+    user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session.userId))
+      .limit(1);
+  } catch (err) {
+    console.log("Failed to get current user data: ", err);
+
+    return NextResponse.json(
+      { success: false, message: "Unexpect error occour" },
+      { status: 500 }
+    );
+  }
+
+  if (user.length === 0) {
+    return NextResponse.json(
+      { success: false, message: "User not found" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json(user[0]);
+}
